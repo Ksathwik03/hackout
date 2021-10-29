1616f9d74e40daa5cda4897ce0939de637d7afad
Ksathwik03 <iit2020241@iiita.ac.in>
2021-10-29 14:56:04 +0530

    login functionality

--- server/app.js
+++ server/app.js
@@ -1,21 +1,43 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const path = require("path");
 const cors = require('cors')
-const env = require('dotenv')
-const PORT = process.env.PORT || 3001
 const app = express()
-env.config()
+app.use(cors({
+    origin: '*'
+}));
 
-app.use(express.json())
-app.use(cors())
 
-// Code Here
+app.use(express.static(path.join(__dirname, "public")));
 
-//Testing code
-app.get('/', (req, res) => {
-    res.send('Hello world! Every thing is working fine')
-})
 
-app.listen(PORT, () => {
-    console.log(`server listening on ${PORT}`);
-})
+// DB CONFIG
+const db = require("./config/keys").MongoURI;
+
+if (db) {
+  mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log(err));
+}
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set("view engine", "ejs");
+
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
+
+app.use("/", require("./Routes/routes.js"));
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
