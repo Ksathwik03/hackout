93e76021fe9811a08d7f2552ea21c33e3e2ed1c6
saisumanthkumar <saisumanthkumar95509@gmail.com>
2021-10-31 02:01:59 +0530

    server

--- server/app.js
+++ server/app.js
@@ -2,9 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const path = require("path");
-const cors = require('cors')
+const cors = require('cors');
+
 const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, { cors: '*' })
+
 app.use(cors({
+
     origin: '*'
 }));
 
@@ -16,10 +21,10 @@ app.use(express.static(path.join(__dirname, "public")));
 const db = require("./config/keys").MongoURI;
 
 if (db) {
-  mongoose
-    .connect(db, { useNewUrlParser: true })
-    .then(() => console.log("MongoDB Connected"))
-    .catch((err) => console.log(err));
+    mongoose
+        .connect(db, { useNewUrlParser: true })
+        .then(() => console.log("MongoDB Connected"))
+        .catch((err) => console.log(err));
 }
 
 app.use(express.urlencoded({ extended: true }));
@@ -27,17 +32,29 @@ app.use(express.json());
 app.set("view engine", "ejs");
 
 app.use(
-  session({
-    secret: "secret",
-    resave: true,
-    saveUninitialized: true,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24,
-    },
-  })
+    session({
+        secret: "secret",
+        resave: true,
+        saveUninitialized: true,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24,
+        },
+    })
 );
 
 app.use("/", require("./Routes/routes.js"));
 
+
+io.on('connection', (socket) => {
+    socket.on('join', (room) => {
+        socket.join(room)
+        socket.emit('message', { user: 'bot', message: 'Welcome user and admin, please use this chat to communicate' })
+    })
+
+    socket.on('sendMessage', (data) => {
+        io.to(data.room).emit('message', { user: data.user, message: data.message })
+    })
+})
+
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+server.listen(PORT, console.log(`Server started on port ${PORT}`));
